4ef8dfb191f2dace081b972a2b37351ba5a7280a
hyperingenious <skbmasale941@gmail.com>
2024-12-25 21:30:49 +0530

    CHANGE: package changed

--- src/parser/compress_image.ts
+++ src/parser/compress_image.ts
@@ -1,11 +1,10 @@
 import imagemin from 'imagemin';
-import * as fs from 'fs';
 import imageminJpegtran from 'imagemin-jpegtran';
 import imageminPngquant from 'imagemin-pngquant';
 
 async function compress_image() {
     // Convert `import.meta.url` to a file path
-    const [{ data }] = await imagemin(['src/ai/*.{jpeg,jpg,png}'], {
+    await imagemin(['src/ai/*.{jpeg,jpg,png}'], {
         destination: `src/parser`, // specify the destination folder
         plugins: [
             imageminJpegtran(),
@@ -14,15 +13,6 @@ async function compress_image() {
             }),
         ],
     });
-
-    // // Saving the image data to a file
-    // fs.writeFile(`${__dirname}/src/parser/output.png`, data, (err) => {
-    //     if (err) {
-    //         console.error('Error saving image:', err);
-    //     } else {
-    //         console.log(`Image saved successfully to `);
-    //     }
-    // })
 }
 
 export { compress_image };
